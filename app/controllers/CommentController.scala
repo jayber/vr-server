0c677453f58295fc5a2c9654e5b0c6d6e627dc2f
James <james.a.bromley@googlemail.com>
2017-05-06 08:43:11 +0100

    comments

--- app/controllers/CommentController.scala
+++ app/controllers/CommentController.scala
@@ -8,7 +8,7 @@ class CommentController extends Controller {
 
   def post(spaceId: String, userId: String) = Action { request =>
     val body = request.body.asFormUrlEncoded.get
-    Logger("client").info(s"userId: $userId, email: ${body("email").head}\n${body("message").head}")
+    Logger("feedback").info(s"userId: $userId, email: ${body("email").head}\n${body("message").head}")
     Ok
   }
 }
